25e83f3c1dedf18e8dd3b135f30595618be9ad4b
Matthew Arbaugh <ma463524@ohio.edu>
2026-09-15 18:08:15 -0400

    This should be it

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Matthew Arbaugh
+ *        Date: 09/01/2026
+ * Description: this was fun? git-lab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+c
+}// main
